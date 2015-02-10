beef9e9bf4e0a22f26ba98a0f97fcbf767755e35
ignacioelola <ignacioelolavillar@gmail.com>
2015-02-10 12:57:04 +0000

    initial commit

---
+++ chain_extractors.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Description: Script to get data using 2 extractor
+# First extractor give urls as output, than are then
+# used as input in the second extractor
+__author__ = 'ignacioelola'
+
+import importio_rsc
