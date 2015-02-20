7a313bc507f19d6b2e27470f2b7ca875ef0f7c3b
ignacioelola <ignacioelolavillar@gmail.com>
2015-02-20 18:40:03 +0000

    firs iteration on dual extractor. Lots of to-do

--- chain_extractors.py
+++ chain_extractors.py
@@ -5,3 +5,25 @@
 __author__ = 'ignacioelola'
 
 import importio_rsc
+
+
+def get_data(guid_level_1, guid_level_2, start_url):
+
+    query_level_1 = {"input": {"webpage/url": start_url}}
+
+    response_level_1 = importio_rsc.query_api(query_level_1, guid_level_1)
+
+    all_results = []
+    for result in response_level_1["results"]:
+        source_url_level_2 = result["url"]
+        print source_url_level_2
+        query_level_2 = {"input": {"webpage/url": source_url_level_2}}
+        response_level_2 = importio_rsc.query_api(query_level_2, guid_level_2)
+
+        results_level_2 = response_level_2.get("results")
+
+        all_results.append({"url": source_url_level_2,
+                            "url/_text": result["url/_text"],
+                            "results": results_level_2})
+
+    return all_results
